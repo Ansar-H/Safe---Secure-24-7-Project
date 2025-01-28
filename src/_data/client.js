52fffab9952064670082e1b34a77310a9efae23e
Ansar-H <ansarh954@gmail.com>
2025-01-28 17:59:47 +0000

    Updated address details for privacy

--- src/_data/client.js
+++ src/_data/client.js
@@ -4,13 +4,9 @@ module.exports = {
   phoneForTel: "44-7442-724-028",
   phoneFormatted: "(44) 7442-724028",
   address: {
-    lineOne: "First Address Line",
-    lineTwo: "Second Address Line",
-    city: "Denver",
-    state: "CO",
-    zip: "80206",
-    country: "US",
-    mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+    city: "Bristol",
+    country: "UK",
+    // Removed full address, state, and ZIP for privacy
   },
   socials: {
     facebook: "https://www.facebook.com/",
